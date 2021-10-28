57ec7b627f3c622954f9f4f0ab693f96235abd74
kkn1125 <xpfjrnseks1@naver.com>
2021-10-28 17:24:55 +0900

    문서화 readme.md 업데이트, 사용법 최신화

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -3,5 +3,5 @@ const documentify = Documentify.init({
     url: 'dist/assets/js/example.js',
     datapath: 'dist/data/userData.json',
     basepath: 'dist/include/',
-    darkMode: true,
+    // darkMode: true,
 });
