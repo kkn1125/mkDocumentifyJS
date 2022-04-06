52becfe213c857a7d6a6908e338da820ca115c0f
kkn1125 <xpfjrnseks1@naver.com>
2022-04-06 20:31:44 +0900

    initial commit

--- dist/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const documentify = Documentify.init({
-    // selectFileMode: true,
-    url: 'dist/assets/js/documentify.js',
-    // datapath: 'dist/data/userData.json',
-    // basepath: 'dist/include/',
-    // darkMode: true,
-    showOrigin: false,
-});
